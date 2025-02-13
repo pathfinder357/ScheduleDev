7c4916a314667bfaa920dc3469397e310c7c9992
pathfinder357 <83073121+pathfinder357@users.noreply.github.com>
2025-02-13 13:16:19 +0900

    Update ScheduleCreateRequestDto.java

--- src/main/java/com/example/schedule/dto/request/ScheduleCreateRequestDto.java
+++ src/main/java/com/example/schedule/dto/request/ScheduleCreateRequestDto.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 
 @Getter
 public class ScheduleCreateRequestDto {
- // json 포맷 정의(property)
     @NotBlank(message = "task는 필수값입니다.")
     private String task;
     @NotBlank(message = "memberEmail은 필수값입니다.")
